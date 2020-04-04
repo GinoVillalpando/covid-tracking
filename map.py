14119707158b3218c4a952e631d7930f05e75d2c
Gino Villalpando <grievxus@outlook.com>
2020-04-04 15:02:01 -0600

    added a different api that just checks for the current postive tests by state to see if my color fill was working with the given data

--- map.py
+++ map.py
@@ -4,19 +4,19 @@ import pandas as pd
 import os
 
 states = os.path.join('data', 'states.json')
-url = 'https://covidtracking.com/api/states'
-covid_data = f'{url}/daily.csv'
+url = 'https://covidtracking.com/api/v1/states'
+covid_data = f'{url}/current.csv'
 
-api = requests.get(f'{url}')
+# this block of code was checking the API for what was stored in the data
+# api = requests.get(f'{url}')
 
+# data = api.json()
 
-data = api.json()
+# for results in data:
+#     print(results['state'])
+#     print(results['positive'])
 
-for results in data:
-    print(results['state'])
-    print(results['positive'])
-
-m = folium.Map(location=[48, -102], zoom_start=3.5)
+MyMap = folium.Map(location=[48, -102], zoom_start=3.5)
 
 folium.Choropleth(
     geo_data=states,
@@ -24,13 +24,13 @@ folium.Choropleth(
     data=pd.read_csv(covid_data),
     columns=['state', 'positive'],
     key_on='feature.id',
-    fill_color='BuPu',
+    fill_color='OrRd',
     fill_opacity=0.7,
     line_opacity=0.2,
     legend_name='Positive Covid Results #'
-).add_to(m)
+).add_to(MyMap)
 
-folium.LayerControl().add_to(m)
+folium.LayerControl().add_to(MyMap)
 
 
-m.save('map.html')
+MyMap.save('map.html')
