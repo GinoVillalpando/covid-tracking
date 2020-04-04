386fe0c6931fc86ff275ceb0168e19df269a1fc0
Gino Villalpando <grievxus@outlook.com>
2020-04-04 11:51:59 -0600

    was able toget data from covidtracker api and print results

--- map.py
+++ map.py
@@ -1,4 +1,12 @@
 import folium
+import requests
+
+api1 = requests.get("https://randomuser.me/api/?results=10")
+
+data = api1.json()
+
+for user in data['results']:
+    print(user['name']['first'])
 
 m = folium.Map(location=[35.0844, -106.6504], zoom_start=12)
 
