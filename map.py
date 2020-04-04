71c2669e8665c5031213bfdd6b9ad39ce278033c
Gino Villalpando <grievxus@outlook.com>
2020-04-04 14:06:07 -0600

    added choropleth method but having issues with the 'positive' column

--- map.py
+++ map.py
@@ -1,7 +1,11 @@
 import folium
 import requests
+import pandas as pd
+import os
 
+states = os.path.join('data', 'states.json')
 url = 'https://covidtracking.com/api/states'
+covid_data = pd.read_csv(f'{url}/daily.csv')
 
 api = requests.get(f'{url}')
 
@@ -15,8 +19,18 @@ for results in data:
 m = folium.Map(location=[48, -102], zoom_start=3.5)
 
 m.choropleth(
-    geo_data=states
-)
+    geo_data=states,
+    name='choropleth',
+    data=covid_data,
+    columns=['state', 'postive'],
+    key_on='feature.id',
+    fill_color='YlGn',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Positive Covid Results #'
+).add_to(m)
+
+folium.LayerControl().add_to(m)
 
 
 m.save('map.html')
