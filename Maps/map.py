08be6f28dd812f402d9f87712cb3ae81a2e2a021
Gino Villalpando <grievxus@outlook.com>
2020-04-04 21:55:05 -0600

    moved data folder to accurately refer to the states.json and changed quantile of legend and colors to represent the postive cases better

--- Maps/map.py
+++ Maps/map.py
@@ -3,13 +3,14 @@
 #   author: Gino Villalpando GinoVillalpandoWork@gmail.com
  
 import folium
-import requests
 import pandas as pd
 import os
 
 states = os.path.join('data', 'states.json')
 url = 'https://covidtracking.com/api/v1/states'
-covid_data = f'{url}/daily.csv'
+covid_data = pd.read_csv(f'{url}/daily.csv')
+
+bins = list(covid_data['positive'].quantile([0, 0.1, 0.2, 0.3, 0.4, 0.5]))
 
 # this block of code was checking the API for what was stored in the data
 # api = requests.get(f'{url}')
@@ -20,19 +21,22 @@ covid_data = f'{url}/daily.csv'
 #     print(results['state'])
 #     print(results['positive'])
 
-MyMap = folium.Map(location=[48, -102], zoom_start=3.5)
+MyMap = folium.Map(location=[48, -102], zoom_start=3)
 
 folium.Choropleth(
     geo_data=states,
     name='choropleth',
-    data=pd.read_csv(covid_data),
+    data=covid_data,
     columns=['state', 'positive'],
     key_on='feature.id',
     fill_color='OrRd',
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Positive Covid Results #'
+    fill_opacity=0.8,
+    line_opacity=0.4,
+    legend_name='Positive COVID-19 Results as of today #',
+    bins=bins,
+    reset=True
 ).add_to(MyMap)
 
+folium.LayerControl().add_to(MyMap)
 
 MyMap.save('map.html')
