a52f1ac681fb35022a68cb5ae096d20ffc8d1ff9
Gino Villalpando <grievxus@outlook.com>
2020-04-13 14:56:44 -0600

    manipulated JS created by map.py to change the range of the map legend

--- Docs/Maps/map.py
+++ Docs/Maps/map.py
@@ -10,6 +10,8 @@ states = os.path.join('data', 'states.json')
 url = 'https://covidtracking.com/api/v1/states'
 covid_data = pd.read_csv(f'{url}/current.csv')
 
+bins = list(covid_data['positive'].quantile([0, 0.25, 0.5, 0.75, 1]))
+
 # create the map at given location with a current value for zoom using folium
 MyMap = folium.Map(location=[48, -102], zoom_start=3)
 
@@ -19,12 +21,13 @@ folium.Choropleth(
     name='choropleth',
     data=covid_data,
     columns=['state', 'positive'],
-    threshold_scale=[0, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000, 200000],
+    # threshold_scale=[0, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000, 200000],
     key_on='feature.id',
     fill_color='BuPu',
     fill_opacity=0.8,
     line_opacity=0.4,
     legend_name='Positive COVID-19 Results as of today #',
+    bins=bins,
     reset=True
 ).add_to(MyMap)
 
