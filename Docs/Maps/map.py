22728147343892bbe5c95c8d9d34d2bd59e9bfe8
Gino Villalpando <grievxus@outlook.com>
2020-04-13 12:57:25 -0600

    created documentation for map.py

--- Docs/Maps/map.py
+++ Docs/Maps/map.py
@@ -10,19 +10,10 @@ states = os.path.join('data', 'states.json')
 url = 'https://covidtracking.com/api/v1/states'
 covid_data = pd.read_csv(f'{url}/current.csv')
 
-# bins = list(covid_data['positive'].quantile([0, 0.25, 0.5, 0.75, 1]))
-
-# this block of code was checking the API for what was stored in the data
-# api = requests.get(f'{url}')
-
-# data = api.json()
-
-# for results in data:
-#     print(results['state'])
-#     print(results['positive'])
-
+# create the map at given location with a current value for zoom using folium
 MyMap = folium.Map(location=[48, -102], zoom_start=3)
 
+# create the Choropleth map with the given data in ../data/states.json
 folium.Choropleth(
     geo_data=states,
     name='choropleth',
@@ -34,10 +25,11 @@ folium.Choropleth(
     fill_opacity=0.8,
     line_opacity=0.4,
     legend_name='Positive COVID-19 Results as of today #',
-    # bins=bins,
     reset=True
 ).add_to(MyMap)
 
+# add layercontrol that will disable/enable choropleth 
 folium.LayerControl().add_to(MyMap)
 
+# create the maps and insert into a html file
 MyMap.save('map.html')
