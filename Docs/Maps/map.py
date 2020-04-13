9387deef9b8dca1c3ba58bf8dd1dd630f7fdb5f8
Gino Villalpando <grievxus@outlook.com>
2020-04-13 12:54:05 -0600

    created threshold_scale values instead of using bins. the data is still represented from the API

--- Docs/Maps/map.py
+++ Docs/Maps/map.py
@@ -10,7 +10,7 @@ states = os.path.join('data', 'states.json')
 url = 'https://covidtracking.com/api/v1/states'
 covid_data = pd.read_csv(f'{url}/current.csv')
 
-bins = list(covid_data['positive'].quantile([0, 0.25, 0.5, 0.75, 1]))
+# bins = list(covid_data['positive'].quantile([0, 0.25, 0.5, 0.75, 1]))
 
 # this block of code was checking the API for what was stored in the data
 # api = requests.get(f'{url}')
@@ -28,15 +28,16 @@ folium.Choropleth(
     name='choropleth',
     data=covid_data,
     columns=['state', 'positive'],
+    threshold_scale=[0, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000, 200000],
     key_on='feature.id',
     fill_color='BuPu',
     fill_opacity=0.8,
     line_opacity=0.4,
     legend_name='Positive COVID-19 Results as of today #',
-    bins=bins,
+    # bins=bins,
     reset=True
 ).add_to(MyMap)
 
-# folium.LayerControl().add_to(MyMap)
+folium.LayerControl().add_to(MyMap)
 
 MyMap.save('map.html')
